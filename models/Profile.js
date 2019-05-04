f122e134c3ee50fb9cedbafa6b961d1d4b23c516
Thao Nguyen <thaonguyen.au1@gmail.com>
2019-05-04 23:04:41 +0700

    Create and update profile

--- models/Profile.js
+++ models/Profile.js
@@ -1,55 +1,114 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({
+const ProfileSchema = new Schema({
   user: {
-    type: Schema.Types.ObjectId,
-    ref: 'users'
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
   },
-  text: {
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
     type: String,
     required: true
   },
-  name: {
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
     type: String
   },
-  avatar: {
+  githubusername: {
     type: String
   },
-  likes: [
+  experience: [
     {
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
-      }
-    }
-  ],
-  comments: [
-    {
-      user: {
-        type: Schema.Types.ObjectId,
-        ref: 'users'
-      },
-      text: {
+      title: {
         type: String,
         required: true
       },
-      name: {
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
         type: String
       },
-      avatar: {
-        type: String
-      },
-      date: {
+      from: {
         type: Date,
-        default: Date.now
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
       }
     }
   ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
   date: {
     type: Date,
     default: Date.now
   }
 });
 
-module.exports = Post = mongoose.model('post', PostSchema);
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
